a627b8a0a5575c43b5c1341d396724c041896a7c
AyanleHashi <ahashi2000@gmail.com>
2016-07-26 19:57:10 -0400

    Update Series UID Comparer V3.py

--- Series UID Comparer V3.py
+++ Series UID Comparer V3.py
@@ -1,10 +1,34 @@
 import os
+import csv
+import difflib
 
 path_basename = []
+UIDList = []
+difflist = []
+
+with open('Series Instance UID List.csv') as uid_csv:
+    csvreader = csv.reader(uid_csv)
+    for i in csvreader:
+        for x in i:
+            UIDList.append(x)
+
+while '' in UIDList:
+    UIDList.remove('')
+UIDList.sort()
 location = os.getcwd()
-print location
+location = 'C:\Users\Ayanle\AppData\Local\Temp\REMBRANDT\900-00-1961'
 
 for (path, dirs, files) in os.walk(location, topdown=True):
             if not dirs:
                 path_basename.append(os.path.basename(path))
-print path_basename
+path_basename.sort()
+
+d = difflib.Differ()
+diff = d.compare(UIDList, path_basename)
+difference = '\n'.join(diff)
+
+difflines = difference.splitlines()
+
+if '+' or '-' in difference:
+    print 'There was a difference:'
+    print difference
