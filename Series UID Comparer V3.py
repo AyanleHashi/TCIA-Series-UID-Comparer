cab8a785cd8093458d0254bee40f0cfc5d6c2ede
Ayanle Hashi <ahashi2000@gmail.com>
2016-07-28 10:02:13 -0400

    Update Series UID Comparer V3.py

--- Series UID Comparer V3.py
+++ Series UID Comparer V3.py
@@ -8,7 +8,7 @@ uid_list = []
 for name in os.listdir(os.getcwd()):
     if name.endswith('.csv'):
         csvname = name
-print "Opening " + csvname + "..."
+print("Opening " + csvname + "...")
 with open(csvname) as uid_csv:
     csvreader = csv.reader(uid_csv)
     for i in csvreader:
@@ -18,12 +18,12 @@ with open(csvname) as uid_csv:
 while '' in uid_list:
     uid_list.remove('')
 uid_list.sort()
-
+print("Scanning downloaded files...")
 for (path, dirs, files) in os.walk(os.getcwd()):
             if not dirs:
                 path_basename.append(os.path.basename(path))
 path_basename.sort()
-
+print("Comparing...")
 d = difflib.Differ()
 diff = d.compare(uid_list, path_basename)
 difference = '\n'.join(diff)
