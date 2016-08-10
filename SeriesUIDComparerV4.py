d7336eca880111f7f43347c0ac78d05795da9a1d
Ayanle Hashi <ahashi2000@gmail.com>
2016-08-10 14:15:39 -0400

    Update SeriesUIDComparerV4.py

--- SeriesUIDComparerV4.py
+++ SeriesUIDComparerV4.py
@@ -1,4 +1,3 @@
-#Updated to read from .dcm files directly instead of folder names
 from __future__ import print_function
 import os
 import dicom
@@ -19,8 +18,9 @@ uid = list(set(uid))#Remove duplicates from list
 uid.sort()
 
 for name in os.listdir(os.getcwd()):#Look for series instance UID .csv file
-    if name.endswith('.csv'):
+    if name.endswith('.csv') and 'MissingCSVs' not in name:
         csvname = name
+print('Opening ' + csvname + '...')
 with open(csvname) as csvfile:#Read csv file
     csvreader = csv.reader(csvfile)
     for i in csvreader:
@@ -29,11 +29,20 @@ with open(csvname) as csvfile:#Read csv file
     csvlist.sort()
 while '' in csvlist:
     csvlist.remove('')#Remove whitespaces
+csvlist.remove(csvlist[2])
+csvlist.remove(csvlist[5])
+csvlist.remove(csvlist[8])
+csvlist.remove(csvlist[10])
 if uid == csvlist:#Check the difference between the csv and extracted data
     print('All files are present.')
 else:
-    d = difflib.Differ()
+    d = difflib.Differ()#If there is a difference, compare the local csv to the extracted data
     diff = d.compare(uid, csvlist)
     difference = '\n'.join(diff).splitlines()
     missing = [m for m in difference if '-' in m]
-    print('You are missing some files. Here is a list of the missing ones:\n', [i for i in missing])
+    print('You are missing some files. Here is a list of the missing ones:\n', missing)
+    
+    with open('MissingCSVs.csv','wb') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        for i in range(len(missing)):
+            csvwriter.writerow([missing[i]])
